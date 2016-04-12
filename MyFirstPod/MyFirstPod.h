c7d8e0fe241c68ff230a0ef936a7b6e481db9b3e
Martin Deandreis <martind@onetree.com>
2016-04-12 11:25:41 -0300

    Initial Commit

---
+++ MyFirstPod/MyFirstPod.h
@@ -0,0 +1,19 @@
+//
+//  MyFirstPod.h
+//  MyFirstPod
+//
+//  Created by Martin dendreis on 4/12/16.
+//  Copyright © 2016 Martin dendreis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFirstPod.
+FOUNDATION_EXPORT double MyFirstPodVersionNumber;
+
+//! Project version string for MyFirstPod.
+FOUNDATION_EXPORT const unsigned char MyFirstPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstPod/PublicHeader.h>
+
+
